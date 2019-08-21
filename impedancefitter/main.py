086f04b2fed98f9b70d54783d27dfe8026c92f9e
Julius Zimmermann <julius.zimmermann@uni-rostock.de>
2019-08-21 17:19:17 +0200

    separate postprocessing

--- impedancefitter/main.py
+++ impedancefitter/main.py
@@ -19,9 +19,6 @@
 import logging
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-import openturns as ot
-from openturns.viewer import View
 
 from lmfit import minimize, Parameters
 import lmfit
@@ -51,8 +48,9 @@ For the documentation, check ../latex/documentation_python.tex
 
 
 class Fitter(object):
-    def __init__(self, **kwargs):
+    def __init__(self, directory=None, **kwargs):
         """
+        provide directory if you run code from directory different to data directory
         provide kwargs as:
             model='SingleShell' OR 'DoubleShell'
             LogLevel='DEBUG' OR 'INFO'
@@ -91,8 +89,9 @@ class Fitter(object):
             self.data_sets = kwargs['data_sets']
         except KeyError:
             self.data_sets = None
-
-        self.directory = os.getcwd() + '/'
+        if directory is None:
+            directory = os.getcwd()
+        self.directory = directory + '/'
         logger.setLevel(self.LogLevel)
 
         # create console handler and set level to debug
@@ -103,7 +102,7 @@ class Fitter(object):
     def main(self, protocol=None):
         max_rows_tag = False
         self.protocol = protocol
-        open(self.directory + 'outfile.yaml', 'w')  # create output file
+        open('outfile.yaml', 'w')  # create output file
         for filename in os.listdir(self.directory):
             filename = os.fsdecode(filename)
             if filename.endswith(self.excludeEnding):
@@ -140,109 +139,6 @@ class Fitter(object):
         txt_file.close()
         logger.debug('number of rows per trace is: ' + str(max_rows))
         return max_rows
-
-    def prepare_post_processing(self):
-        '''
-        draws pdf of all calculated parameters
-        '''
-        file = open(self.directory + 'outfile.yaml', 'r')
-        data = yaml.safe_load(file)
-        alphalist, emlist, klist, kmlist, kcplist, kmedlist, emedlist, enelist, knelist, knplist = ([] for i in range(10))
-        for key in data:
-            alphalist.append([data[key]['alpha']])
-            emlist.append([data[key]['em']])
-            klist.append([data[key]['k']])
-            kmlist.append([data[key]['km']])
-            kcplist.append([data[key]['kcp']])
-            kmedlist.append([data[key]['kmed']])
-            emedlist.append([data[key]['emed']])
-            if(self.model == 'DoubleShell'):
-                enelist.append([data[key]['ene']])
-                knelist.append([data[key]['kne']])
-                knplist.append([data[key]['knp']])
-        # write data into dict
-        self.sampledict = {}
-        self.sampledict['alpha'] = ot.Sample(np.array(alphalist))
-        self.sampledict['em'] = ot.Sample(np.array(emlist))
-        self.sampledict['k'] = ot.Sample(np.array(klist))
-        self.sampledict['km'] = ot.Sample(np.array(kmlist))
-        self.sampledict['kcp'] = ot.Sample(np.array(kcplist))
-        self.sampledict['kmed'] = ot.Sample(np.array(kmedlist))
-        self.sampledict['emed'] = ot.Sample(np.array(emedlist))
-        if(self.model == 'DoubleShell'):
-            self.sampledict['ene'] = ot.Sample(np.array(enelist))
-            self.sampledict['kne'] = ot.Sample(np.array(knelist))
-            self.sampledict['knp'] = ot.Sample(np.array(knplist))
-
-    def plot_histograms(self):
-        """
-        fails if values are too close to each other
-        """
-        if(self.model == 'SingleShell'):
-            fig, ax = plt.subplots(nrows=2, ncols=3)
-        else:
-            fig, ax = plt.subplots(nrows=3, ncols=3)
-        r = 0
-        c = 0
-        for key in self.sampledict:
-            graph = ot.HistogramFactory().build(self.sampledict[key]).drawPDF()
-            graph.setTitle("Histogram for variables")
-            graph.setXTitle(key)
-            View(graph, axes=[ax[r, c]], plot_kwargs={'label': key, 'c': 'black'})
-            kernel = ot.KernelSmoothing()
-            graph_k = kernel.build(self.sampledict[key])
-            graph_k = graph_k.drawPDF()
-            graph_k.setTitle("Histogram for variables")
-            graph_k.setXTitle(key)
-            View(graph_k, axes=[ax[r, c]], plot_kwargs={'label': key})
-            # jump to next ax object or next row
-            c += 1
-            if(c == 3):
-                c = 0
-                r += 1
-        plt.tight_layout()
-        plt.show()
-
-    def fit_to_normal_distribution(self, parameter):
-        sample = self.sampledict[parameter]
-        distribution = ot.NormalFactory().build(sample)
-        print(distribution)
-        # Draw QQ plot to check fitted distribution
-        QQ_plot = ot.VisualTest.DrawQQplot(sample, distribution)
-        View(QQ_plot).show()
-        return distribution
-
-    def fit_to_histogram_distribution(self, parameter):
-        sample = self.sampledict[parameter]
-        distribution = ot.HistogramFactory().build(sample)
-        print(distribution)
-        # Draw QQ plot to check fitted distribution
-        QQ_plot = ot.VisualTest.DrawQQplot(sample, distribution)
-        View(QQ_plot).show()
-        return distribution
-
-    def best_model_kolmogorov(self, parameter):
-        """
-        suitable for small samples
-        """
-        sample = self.sampledict[parameter]
-        tested_distributions = [ot.NormalFactory(), ot.UniformFactory()]
-        best_model, best_result = ot.FittingTest.BestModelKolmogorov(sample, tested_distributions)
-        logger.info("Best model:")
-        logger.info(best_model)
-        if self.LogLevel == 'DEBUG':
-            logger.debug("QQ Plot for best model:")
-            QQ_plot = ot.VisualTest.DrawQQplot(sample, best_model)
-            View(QQ_plot).show()
-        return best_model
-
-    def best_model_chisquared(self, parameter):
-        sample = self.sampledict[parameter]
-        tested_distributions = [ot.ExponentialFactory(), ot.NormalFactory(), ot.BetaFactory(), ot.UniformFactory(), ot.TruncatedNormalFactory()]
-        best_model, best_result = ot.FittingTest.BestModelChiSquared(sample, tested_distributions)
-        logger.info("Best model:")
-        logger.info(best_model)
-        return best_model
 
     def readin_Data_from_xlsx(self, filename):
         """
@@ -291,7 +187,7 @@ class Fitter(object):
         for key in values:
             values[key] = values.get(key).item()  # conversion into python native type
         data = {str(filename): values}
-        outfile = open(self.directory + 'outfile.yaml', 'a')  # append to the already existing file, or create it, if ther is no file
+        outfile = open('outfile.yaml', 'a')  # append to the already existing file, or create it, if ther is no file
         yaml.dump(data, outfile)
 
     def prepare_txt(self):
