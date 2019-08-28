24caf0530171ec82e5eb525ad7fc77926c308776
Julius Zimmermann <julius.zimmermann@uni-rostock.de>
2019-08-28 12:00:10 +0200

    added new lmfit prerelease

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    dependency_links=['https://github.com/lmfit/lmfit-py/archive/41667df8052d2e9102076c49eea008e03a20ca1b.zip#egg=lmfit-0.9.13+git.41667df'],
-    install_requires=['pyyaml', 'lmfit==0.9.13+git.41667df',
+    dependency_links=['https://github.com/lmfit/lmfit-py/tarball/0.9.14rc1#egg=lmfit-0.9.14rc1'],
+    install_requires=['pyyaml', 'lmfit==0.9.14rc1',
                       'numpy>=1.16', 'xlrd>=1.2.0', 'openturns>=1.12'],
 )
