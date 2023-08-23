7c28eac946ebb479d5a0b248944ae3ccef77cfae
Julius Zimmermann <julius.zimmermann@uni-rostock.de>
2023-08-23 17:41:06 +0200

    Pythn 3.7 EOL, new Python 3.11

--- setup.py
+++ setup.py
@@ -26,5 +26,5 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=install_requires,
     package_data={"": ["LICENSE"]},
-    python_requires='>=3.7',
+    python_requires='>=3.8',
 )
