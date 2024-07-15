2c03f86cfd97b619d6bcd037e7fdf615a3bd81cb
Julius Zimmermann <43932404+j-zimmermann@users.noreply.github.com>
2024-07-16 00:27:13 +0200

    coding style black

--- impedancefitter/RC.py
+++ impedancefitter/RC.py
@@ -1,4 +1,5 @@
-#    The ImpedanceFitter is a package to fit impedance spectra to equivalent-circuit models using open-source software.
+#    The ImpedanceFitter is a package to fit impedance spectra to
+#    equivalent-circuit models using open-source software.
 #
 #    Copyright (C) 2018, 2019 Leonard Thiele, leonard.thiele[AT]uni-rostock.de
 #    Copyright (C) 2018, 2019 Julius Zimmermann, julius.zimmermann[AT]uni-rostock.de
@@ -25,7 +26,6 @@ def RC_model(omega, Rd, Cd):
 
     Parameters
     ----------
-
     omega: :class:`numpy.ndarray`, double
         list of frequencies
     Rd: double
@@ -35,6 +35,7 @@ def RC_model(omega, Rd, Cd):
 
     Notes
     -----
+    Pay attention to the units.
     .. warning::
 
         `Cd` is in pF!
@@ -45,7 +46,7 @@ def RC_model(omega, Rd, Cd):
         Impedance array
     """
     Cfit = Cd * 1e-12
-    Z_fit = Rd / (1. + 1j * omega * Cfit * Rd)
+    Z_fit = Rd / (1.0 + 1j * omega * Cfit * Rd)
     return Z_fit
 
 
@@ -53,8 +54,7 @@ def rc_model(omega, c0, kdc, eps):
     """Simple RC model of a lossy dielectric to obtain dielectric properties.
 
     Parameters
-    -----------
-
+    ----------
     omega: :class:`numpy.ndarray`, double
         list of frequencies
     c0: double
@@ -72,7 +72,6 @@ def rc_model(omega, c0, kdc, eps):
 
     Notes
     -----
-
     .. warning::
 
         C0 is in pF!
@@ -82,7 +81,7 @@ def rc_model(omega, c0, kdc, eps):
     Rd = e0 / (kdc * c0)
     # Cd = eps * c0
     factor = eps * e0 / kdc  # Cd * Rd
-    Z_fit = Rd / (1. + 1j * omega * factor)
+    Z_fit = Rd / (1.0 + 1j * omega * factor)
     return Z_fit
 
 
@@ -90,8 +89,7 @@ def drc_model(omega, RE, tauE, alpha, beta):
     """Distributed RC circuit.
 
     Parameters
-    -----------
-
+    ----------
     omega: :class:`numpy.ndarray`, double
         list of frequencies
     RE: double
@@ -119,14 +117,14 @@ def drc_model(omega, RE, tauE, alpha, beta):
 
     References
     ----------
-
-    .. [Emmert2011] Emmert, S., Wolf, M., Gulich, R., Krohns, S., Kastner, S., Lunkenheimer, P., & Loidl, A. (2011).
+    .. [Emmert2011] Emmert, S., Wolf, M., Gulich, R., Krohns, S.,
+                    Kastner, S., Lunkenheimer, P., & Loidl, A. (2011).
                     Electrode polarization effects in broadband dielectric spectroscopy.
                     European Physical Journal B, 83(2), 157–165.
                     https://doi.org/10.1140/epjb/e2011-20439-8
     """
     tauE *= 1e-9
-    nom = power(1. + power(1j * omega * tauE, 1. - alpha), beta)
+    nom = power(1.0 + power(1j * omega * tauE, 1.0 - alpha), beta)
     return RE / nom
 
 
@@ -134,8 +132,7 @@ def rc_tau_model(omega, Rk, tauk):
     r"""Compute RC model without explicit capacitance.
 
     Parameters
-    -----------
-
+    ----------
     omega: :class:`numpy.ndarray`, double
         list of frequencies
     Rk: double
@@ -143,8 +140,6 @@ def rc_tau_model(omega, Rk, tauk):
     tauk: double
         relaxation time
 
-    Notes
-    -----
     Returns
     -------
     :class:`numpy.ndarray`, complex
@@ -161,5 +156,4 @@ def rc_tau_model(omega, Rk, tauk):
         Z = \frac{R_\mathrm{k}}{1+ j \omega \tau_\mathrm{k}}
 
     """
-
-    return Rk / (1. + 1j * omega * tauk)
+    return Rk / (1.0 + 1j * omega * tauk)
