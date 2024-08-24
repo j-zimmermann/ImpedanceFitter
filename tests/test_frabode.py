918a0f0cfbf2f9859ada2cad557c946e22d577dc
Julius Zimmermann <43932404+j-zimmermann@users.noreply.github.com>
2024-08-25 00:38:40 +0200

    enable test runs

--- tests/test_frabode.py
+++ tests/test_frabode.py
@@ -15,7 +15,7 @@ def test_bode_to_impedance():
 def test_RandS_bode_csv_to_impedance():
     """Test correct frequency conversion."""
     omega, impedance = bode_csv_to_impedance(
-        "impedancefitter/tests/RohdeSchwarz_47R_100R_shunt.csv", "R&S"
+        "tests/RohdeSchwarz_47R_100R_shunt.csv", "R&S"
     )
     omega_test = 2.0 * np.pi * np.logspace(1, 7, num=301)
     assert np.all(np.isclose(omega, omega_test, rtol=1e-2))
@@ -24,7 +24,7 @@ def test_RandS_bode_csv_to_impedance():
 def test_MokuGo_bode_csv_to_impedance():
     """Test correct frequency conversion."""
     omega, impedance = bode_csv_to_impedance(
-        "impedancefitter/tests/MokuGo_47R_100R_shunt.csv", "MokuGo"
+        "tests/MokuGo_47R_100R_shunt.csv", "MokuGo"
     )
     omega_test = 2.0 * np.pi * np.logspace(1, np.log10(2e7), num=512)
     assert np.all(np.isclose(omega, omega_test))
