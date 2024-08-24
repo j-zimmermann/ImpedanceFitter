5a1d8466b4715514ce4dae62832432b1779ff2a7
Julius Zimmermann <43932404+j-zimmermann@users.noreply.github.com>
2024-08-24 23:10:37 +0200

    CPE formatted with black and ruff

--- src/impedancefitter/cpe.py
+++ src/impedancefitter/cpe.py
@@ -1,7 +1,9 @@
-#    The ImpedanceFitter is a package to fit impedance spectra to equivalent-circuit models using open-source software.
+#    The ImpedanceFitter is a package to fit impedance spectra to
+#    equivalent-circuit models using open-source software.
 #
 #    Copyright (C) 2018, 2019 Leonard Thiele, leonard.thiele[AT]uni-rostock.de
-#    Copyright (C) 2018, 2019, 2020 Julius Zimmermann, julius.zimmermann[AT]uni-rostock.de
+#    Copyright (C) 2018, 2019, 2020 Julius Zimmermann,
+#                                   julius.zimmermann[AT]uni-rostock.de
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -16,8 +18,9 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from .elements import Z_CPE, Z_w, parallel
 import numpy as np
+
+from .elements import Z_CPE, Z_w, parallel
 
 
 def cpe_model(omega, k, alpha):
@@ -35,6 +38,7 @@ def cpe_model(omega, k, alpha):
         CPE factor
     alpha: double
         CPE phase
+
     Returns
     -------
     :class:`numpy.ndarray`, complex
@@ -59,12 +63,12 @@ def cpetissue_model(omega, k, alpha):
         CPE factor
     alpha: double
         CPE phase
+
     Returns
     -------
     :class:`numpy.ndarray`, complex
         Impedance array
     """
-
     k *= 1e3
     Z_fit = Z_CPE(omega, k, alpha)
     return Z_fit
@@ -84,6 +88,7 @@ def cpe_ct_model(omega, k, alpha, Rct):
         CPE phase
     Rct: double
         charge transfer resistance
+
     Returns
     -------
     :class:`numpy.ndarray`, complex
@@ -94,7 +99,6 @@ def cpe_ct_model(omega, k, alpha, Rct):
     :meth:`impedancefitter.cpe.cpe_model`
 
     """
-
     Z_fit = parallel(Z_CPE(omega, k, alpha), Rct)
     return Z_fit
 
@@ -113,6 +117,7 @@ def cpe_ct_tissue_model(omega, k, alpha, Rct):
         CPE phase
     Rct: double
         charge transfer resistance
+
     Returns
     -------
     :class:`numpy.ndarray`, complex
@@ -166,7 +171,6 @@ def cpe_onset_model(omega, f0, nu, Z0):
 
     Notes
     -----
-
     .. math::
 
         Z_\mathrm{CPE} = Z_0 \left(j \frac{\omega}{2 \pi f_0} \right)^{-\nu}
@@ -177,10 +181,12 @@ def cpe_onset_model(omega, f0, nu, Z0):
     ----------
     omega: :class:`numpy.ndarray`
         List of frequencies.
-    k: double
-        CPE factor
-    alpha: double
+    f0: double
+        center frequency
+    nu: double
         CPE phase
+    Z0: double
+        CPE amplitude
 
     Returns
     -------
@@ -189,11 +195,12 @@ def cpe_onset_model(omega, f0, nu, Z0):
 
     References
     ----------
-
-    .. [Ishai2012] Ishai, P. Ben, Sobol, Z., Nickels, J. D., Agapov, A. L., & Sokolov, A. P. (2012).
-                   An assessment of comparative methods for approaching electrode polarization in dielectric permittivity measurements.
+    .. [Ishai2012] Ishai, P. Ben, Sobol, Z., Nickels, J. D., Agapov, A. L.,
+                   & Sokolov, A. P. (2012).
+                   An assessment of comparative methods for approaching electrode
+                   polarization in dielectric permittivity measurements.
                    Review of Scientific Instruments, 83(8).
                    https://doi.org/10.1063/1.4746992
     """
-    Z_fit = Z0 * np.power(1j * omega / (2. * np.pi * f0), -nu)
+    Z_fit = Z0 * np.power(1j * omega / (2.0 * np.pi * f0), -nu)
     return Z_fit
