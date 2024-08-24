d5367e22541cb4c8cbed8ff053c1437a0532d876
Julius Zimmermann <43932404+j-zimmermann@users.noreply.github.com>
2024-08-24 22:40:19 +0200

    elements formatted with black and ruff

--- src/impedancefitter/elements.py
+++ src/impedancefitter/elements.py
@@ -1,7 +1,9 @@
-#    The ImpedanceFitter is a package to fit impedance spectra to equivalent-circuit models using open-source software.
+#    The ImpedanceFitter is a package to fit impedance spectra to
+#    equivalent-circuit models using open-source software.
 #
 #    Copyright (C) 2018, 2019 Leonard Thiele, leonard.thiele[AT]uni-rostock.de
-#    Copyright (C) 2018, 2019, 2020 Julius Zimmermann, julius.zimmermann[AT]uni-rostock.de
+#    Copyright (C) 2018, 2019, 2020 Julius Zimmermann,
+#                                   julius.zimmermann[AT]uni-rostock.de
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -20,7 +22,7 @@ import numpy as np
 
 
 def log(Z1, dummy):
-    """Return logarithm of impedance for LMFIT
+    """Return logarithm of impedance for LMFIT.
 
     Parameters
     ----------
@@ -29,13 +31,12 @@ def log(Z1, dummy):
     dummy : :class:`numpy.ndarray`, complex
         Array full of ones, such that np.log10(dummy) is zero.
     """
-
     dummy = np.ones(Z1.shape)
     return np.log10(Z1) + np.log10(dummy)
 
 
 def eps(Z1, make_eps):
-    """Return complex permittivity for LMFIT
+    """Return complex permittivity for LMFIT.
 
     Parameters
     ----------
@@ -44,7 +45,6 @@ def eps(Z1, make_eps):
     make_eps : :class:`numpy.ndarray`, complex
         Output of function :func:`impedancefitter.utils.make_eps`
     """
-
     return make_eps / Z1
 
 
@@ -84,7 +84,7 @@ def Z_R(omega, R):
         Impedance array
 
     """
-    new = np.zeros_like(omega, dtype='complex128')
+    new = np.zeros_like(omega, dtype="complex128")
     return new + R
 
 
@@ -109,7 +109,7 @@ def Z_L(omega, L):
 
 def Z_CPE(omega, k, alpha):
     r"""
-    CPE impedance
+    CPE impedance.
 
     .. math::
 
@@ -123,6 +123,7 @@ def Z_CPE(omega, k, alpha):
         CPE factor
     alpha: double
         CPE phase
+
     Returns
     -------
     :class:`numpy.ndarray`, complex
@@ -136,7 +137,7 @@ def Z_CPE(omega, k, alpha):
 
 
 def Z_C(omega, C):
-    """Capacitor impedance
+    """Capacitor impedance.
 
     Parameters
     ----------
@@ -144,19 +145,20 @@ def Z_C(omega, C):
         List of frequencies.
     C: double
         capacitance of capacitor
+
     Returns
     -------
     :class:`numpy.ndarray`, complex
         Impedance array
     """
-    Z = 1. / (1j * omega * C)
+    Z = 1.0 / (1j * omega * C)
     # catch DC case?
     # Z[np.isnan(Z)] = np.inf
     return Z
 
 
 def Z_w(omega, Aw):
-    r"""Warburg element
+    r"""Warburg element.
 
     .. math::
 
@@ -176,11 +178,11 @@ def Z_w(omega, Aw):
         Impedance array
 
     """
-    return Aw * (1. - 1j) / np.sqrt(omega)
+    return Aw * (1.0 - 1j) / np.sqrt(omega)
 
 
 def Z_stray(omega, Cs):
-    """Stray capacitance in pF
+    """Stray capacitance in pF.
 
     Parameters
     ----------
@@ -188,6 +190,7 @@ def Z_stray(omega, Cs):
         List of frequencies.
     Cs: double
         Stray capacitance, for numerical reasons in pF.
+
     Returns
     -------
     :class:`numpy.ndarray`, complex
@@ -197,7 +200,7 @@ def Z_stray(omega, Cs):
 
 
 def Z_ws(omega, Rw, wd):
-    r"""Warburg short element with absorbing boundary
+    r"""Warburg short element with absorbing boundary.
 
     Parameters
     ----------
@@ -216,24 +219,23 @@ def Z_ws(omega, Rw, wd):
 
     Notes
     -----
-
     This element is also referred to as finite-length Warburg element
     with transmissive boundary [Barsoukov2018]_.
     Here, the formulation from [Bisquert2001]_ is used
 
     .. math::
 
-        Z_\mathrm{W} = R_\mathrm{W} \sqrt{\omega_d / (j \omega)} \tanh\left(\sqrt{j \omega / omega_d}\right)
+        Z_\mathrm{W} = R_\mathrm{W} \sqrt{\omega_d / (j \omega)}
+                       \tanh\left(\sqrt{j \omega / omega_d}\right)
 
     is implemented.
 
     """
-
     return Rw * np.sqrt(wd / (1j * omega)) * np.tanh(np.sqrt(1j * omega / wd))
 
 
 def Z_wo(omega, Rw, wd):
-    r"""Warburg open element with reflecting boundary
+    r"""Warburg open element with reflecting boundary.
 
     Parameters
     ----------
@@ -253,24 +255,23 @@ def Z_wo(omega, Rw, wd):
 
     Notes
     -----
-
     This element is also referred to as finite-length Warburg element
     with reflective boundary [Barsoukov2018]_.
     Here, the formulation from [Bisquert2001]_ is used
 
     .. math::
 
-        Z_\mathrm{W} = \frac{R_\mathrm{W} (\omega_d j \omega)^{1/2}}{\tanh\left( \sqrt{j \omega / \omega_d})}
+        Z_\mathrm{W} = \frac{R_\mathrm{W} (\omega_d j \omega)^{1/2}}
+                       {\tanh\left( \sqrt{j \omega / \omega_d}\right)}
 
     is implemented.
 
     """
-
     return Rw * np.sqrt(wd / (1j * omega)) / np.tanh(np.sqrt(1j * omega / wd))
 
 
 def Z_ADIb_r(omega, Rw, wd, gamma):
-    r"""Anomalous diffusion Ib with reflecting boundary
+    r"""Anomalous diffusion Ib with reflecting boundary.
 
     Parameters
     ----------
@@ -292,23 +293,25 @@ def Z_ADIb_r(omega, Rw, wd, gamma):
 
     Notes
     -----
-
     This element is described in [Bisquert2001]_.
 
     References
     ----------
-
     .. [Bisquert2001] Bisquert, J., and Compte, A. (2001).
                    Theory of the electrochemical impedance of anomalous diffusion.
                    Journal of Electroanalytical Chemistry, 499(1), 112–120.
                    https://doi.org/10.1016/S0022-0728(00)00497-6
     """
-
-    return Rw * np.power(wd, gamma - 1.0) * np.power(wd / (1j * omega), 1.0 - 0.5 * gamma) / np.tanh(np.power(1j * omega / wd, 0.5 * gamma))
+    return (
+        Rw
+        * np.power(wd, gamma - 1.0)
+        * np.power(wd / (1j * omega), 1.0 - 0.5 * gamma)
+        / np.tanh(np.power(1j * omega / wd, 0.5 * gamma))
+    )
 
 
 def Z_ADIb_a(omega, Rw, wd, gamma):
-    r"""Anomalous diffusion Ib with absorbing boundary
+    r"""Anomalous diffusion Ib with absorbing boundary.
 
     Parameters
     ----------
@@ -330,16 +333,19 @@ def Z_ADIb_a(omega, Rw, wd, gamma):
 
     Notes
     -----
-
     This element is described in [Bisquert2001]_.
 
     """
-
-    return Rw * np.power(wd, gamma - 1.0) * np.power(wd / (1j * omega), 1.0 - 0.5 * gamma) * np.tanh(np.power(1j * omega / wd, 0.5 * gamma))
+    return (
+        Rw
+        * np.power(wd, gamma - 1.0)
+        * np.power(wd / (1j * omega), 1.0 - 0.5 * gamma)
+        * np.tanh(np.power(1j * omega / wd, 0.5 * gamma))
+    )
 
 
 def Z_ADIa_r(omega, Rw, wd, gamma):
-    r"""Anomalous diffusion Ia with reflecting boundary
+    r"""Anomalous diffusion Ia with reflecting boundary.
 
     Parameters
     ----------
@@ -361,16 +367,18 @@ def Z_ADIa_r(omega, Rw, wd, gamma):
 
     Notes
     -----
-
     This element is described in [Bisquert2001]_.
 
     """
-
-    return Rw * np.power(wd / (1j * omega), 0.5 * gamma) / np.tanh(np.power(1j * omega / wd, 0.5 * gamma))
+    return (
+        Rw
+        * np.power(wd / (1j * omega), 0.5 * gamma)
+        / np.tanh(np.power(1j * omega / wd, 0.5 * gamma))
+    )
 
 
 def Z_ADIa_a(omega, Rw, wd, gamma):
-    r"""Anomalous diffusion Ia with absorbing boundary
+    r"""Anomalous diffusion Ia with absorbing boundary.
 
     Parameters
     ----------
@@ -392,16 +400,18 @@ def Z_ADIa_a(omega, Rw, wd, gamma):
 
     Notes
     -----
-
     This element is described in [Bisquert2001]_.
 
     """
-
-    return Rw * np.power(wd / (1j * omega), 0.5 * gamma) * np.tanh(np.power(1j * omega / wd, 0.5 * gamma))
+    return (
+        Rw
+        * np.power(wd / (1j * omega), 0.5 * gamma)
+        * np.tanh(np.power(1j * omega / wd, 0.5 * gamma))
+    )
 
 
 def Z_ADII_r(omega, Rw, wd, gamma):
-    r"""Anomalous diffusion II with reflecting boundary
+    r"""Anomalous diffusion II with reflecting boundary.
 
     Parameters
     ----------
@@ -423,16 +433,19 @@ def Z_ADII_r(omega, Rw, wd, gamma):
 
     Notes
     -----
-
     This element is described in [Bisquert2001]_.
 
     """
-
-    return Rw * np.power(wd, 1.0 - gamma) * np.power(wd / (1j * omega), 0.5 * gamma) / np.tanh(np.power(1j * omega / wd, 1.0 - 0.5 * gamma))
+    return (
+        Rw
+        * np.power(wd, 1.0 - gamma)
+        * np.power(wd / (1j * omega), 0.5 * gamma)
+        / np.tanh(np.power(1j * omega / wd, 1.0 - 0.5 * gamma))
+    )
 
 
 def Z_ADII_a(omega, Rw, wd, gamma):
-    r"""Anomalous diffusion II with absorbing boundary
+    r"""Anomalous diffusion II with absorbing boundary.
 
     Parameters
     ----------
@@ -454,9 +467,12 @@ def Z_ADII_a(omega, Rw, wd, gamma):
 
     Notes
     -----
-
     This element is described in [Bisquert2001]_.
 
     """
-
-    return Rw * np.power(wd, 1.0 - gamma) * np.power(wd / (1j * omega), 0.5 * gamma) * np.tanh(np.power(1j * omega / wd, 1.0 - 0.5 * gamma))
+    return (
+        Rw
+        * np.power(wd, 1.0 - gamma)
+        * np.power(wd / (1j * omega), 0.5 * gamma)
+        * np.tanh(np.power(1j * omega / wd, 1.0 - 0.5 * gamma))
+    )
